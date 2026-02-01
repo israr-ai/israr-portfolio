5c42009f5424803d55598edad32e22ac5a9088f2
Mdiscode <moisrarmoisrar5960@gmail.com>
2026-02-01 12:45:56 +0530

    change latest project

--- main.js
+++ main.js
@@ -26,18 +26,7 @@ const roles = ["AI Engineer", "Data Scientist", "Frontend Developer", "ML Engine
   });
 
 
-    // --------------------back--to--tap-----------
-     const backToTopBtn = document.getElementById('backToTopBtn');
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 200) {
-      backToTopBtn.classList.remove('opacity-0', 'pointer-events-none');
-      backToTopBtn.classList.add('opacity-100');
-    } else {
-      backToTopBtn.classList.add('opacity-0', 'pointer-events-none');
-      backToTopBtn.classList.remove('opacity-100');
-    }
-  });
+  
 
 
 //   ----------form--submiton-----msd-----------------------------
@@ -68,3 +57,26 @@ const roles = ["AI Engineer", "Data Scientist", "Frontend Developer", "ML Engine
       result.classList.add("text-red-400");
     }
   });
+
+
+
+// project scroll 
+  
+  let currentIndex = 0;
+  const slides = document.getElementById("projectSlides");
+  const totalSlides = slides.children.length;
+
+  function updateSlide() {
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateSlide();
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    updateSlide();
+  }
+
